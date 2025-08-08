3fe8c6f8c02bb9ed1980df54840b4b6583bf9c26
Ygor-Ribas <moreiraribasy@gmail.com>
2025-08-08 15:55:23 -0300

    .

--- AULA 09 - Eventos DOM/desafio03 - media/script.js
+++ AULA 09 - Eventos DOM/desafio03 - media/script.js
@@ -1,7 +1,12 @@
 function mostrarResultado(){
-    let nota1 = parseInt(document.getElementById("nota1").value)
-    let nota2 = parseInt(document.querySelector("#nota2").value)
-    let media = (nota1 + nota2) / 2
-    console.log(media);
+    let numero = parseInt(document.getElementById("numero").value)
+    let soma = numero + 1
+    let subtracao = numero - 1
+
+    console.log("O sucessor de ", numero, "é " +soma);
+    console.log("O antecessor de ", numero, "é " +subtracao);
+    let resultado = document.querySelector("#resultado")
+
+    resultado.innerHTML = "O sucessor de "+ numero + " é " +soma + "<br> O antecessor "+ numero + " é " +subtracao 
     
 }
