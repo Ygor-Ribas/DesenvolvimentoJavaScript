f308c8a219b3d6dc7436a485c28158126c7cdf56
Ygor-Ribas <moreiraribasy@gmail.com>
2025-08-12 15:34:59 -0300

    .

---
+++ AULA 10 - estruturasCondicionais/exemplos01/metodoPagamento/script.js
@@ -0,0 +1,14 @@
+function mostrarResultado(){
+    let valorGasto = parseFloat(document.getElementById("valorGasto").value)
+    let metodoPagamento = document.getElementById("metodoPagamento").value
+    let resultado = document.querySelector("#resultado")
+
+
+    if(metodoPagamento === "1"){
+        let valorFinal = valorGasto * 0.9
+        resultado.innerHTML = "O valor da sua compra ficou R$: " +valorFinal.toFixed(2).replace("." , ",")
+    }else{
+        resultado.innerHTML = "O valor da sua compra ficou R$: " +valorGasto.toFixed(2).replace("." , ",")
+
+    }
+}
