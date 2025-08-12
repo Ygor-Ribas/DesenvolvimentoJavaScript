73c4cd8a8aa02ffdd38d388c32688340b8bcd318
Ygor-Ribas <moreiraribasy@gmail.com>
2025-08-12 14:40:33 -0300

    .

--- AULA 10 - estruturasCondicionais/integrandoHTML/exemplos/script.js
+++ AULA 10 - estruturasCondicionais/integrandoHTML/exemplos/script.js
@@ -2,18 +2,18 @@ function mostrarResultado(){
     let nota1 = parseInt(document.querySelector("#nota1").value)
     let nota2 = parseInt(document.querySelector("#nota2").value)
     let paragrafomedia = document.querySelector("#media")
-    let paragrafoResultado = document.querySelector("#resultado")
+    let paragraforesultado = document.querySelector("#resultado")
 
     let media = (nota1 + nota2) /2
     paragrafomedia.innerHTML = "A sua média foi de " +media
 
     if(media >= 7 ){
-        paragrafoResultado.innerHTML = "Aprovado"
-        paragrafoResultado.computedStyleMap.color ="blue"
+        paragraforesultado.innerHTML = "Aprovado"
+        paragraforesultado.style.color = "green"
         
     }else{
-        paragrafoResultado.innerHTML = "Reprovado"
-        paragrafoResultado.computedStyleMap.color ="red"
+        paragraforesultado.innerHTML = "Reprovado"
+        paragraforesultado.style.color = "red"
 
     }
 }
