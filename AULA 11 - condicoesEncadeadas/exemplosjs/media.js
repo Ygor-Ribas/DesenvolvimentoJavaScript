4e3f1ea1af04c607c376b35da63eff6962c39b58
Ygor-Ribas <moreiraribasy@gmail.com>
2025-08-12 16:53:31 -0300

    .

---
+++ AULA 11 - condicoesEncadeadas/exemplosjs/media.js
@@ -0,0 +1,15 @@
+let nota1 = 100
+let nota2 = 70
+
+let media = (nota1 + nota2) /2 
+
+if(media >= 70){
+    console.log("Aprovado");
+    
+}else if (media >= 50){
+    console.log("Recuperação");
+    
+}else{
+    console.log("reprovado");
+    
+}
