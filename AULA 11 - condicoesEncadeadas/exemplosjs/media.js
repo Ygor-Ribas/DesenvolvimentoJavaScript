fa48ecffc656d035cedb9622bb0e53034f2aafbe
Ygor-Ribas <moreiraribasy@gmail.com>
2025-08-13 14:48:18 -0300

    .

--- AULA 11 - condicoesEncadeadas/exemplosjs/media.js
+++ AULA 11 - condicoesEncadeadas/exemplosjs/media.js
@@ -1,13 +1,25 @@
-let nota1 = 100
-let nota2 = 70
+let nota1 = 50
+let nota2 = 50
 
-let media = (nota1 + nota2) /2 
+let media = (nota1 + nota2) / 2 
 
 if(media >= 70){
     console.log("Aprovado");
     
-}else if (media >= 50){
+}
+else if (media >= 50){
+
     console.log("Recuperação");
+    let recuperacao = 90
+    let mediaFinal = (media + recuperacao) / 2
+
+    if(mediaFinal >= 70){
+        console.log("Aprovado");
+        
+    }else{
+        console.log("Reprovado");
+        
+    }
     
 }else{
     console.log("reprovado");
---
+++ AULA 11 - condicoesEncadeadas/exemplosjs/metodosPagamentos.js
@@ -0,0 +1,45 @@
+// Valor gasto pelo usuario.
+// Metodo de pagamento
+//   - 1 - Boleto/Pix = 10% desconto
+//   - 2 - Credito = Valor total  
+
+
+let valorGasto = 1000
+let metodoPagamento = 0
+let parcelamento = 2
+
+
+if (metodoPagamento == 1) {
+    let valorFinal = valorGasto * 0.9
+    
+    console.log("MÉTODO DE PAGAMENTO BOLETO/PIX")
+    console.log("O valor final foi de R$ " + valorFinal);
+}
+
+else if(metodoPagamento == 2){
+    let valorFinal2 = valorGasto * 0.95
+    console.log("MÉTODO DE PAGAMENTO DÉBITO")
+    console.log("O valor final foi de R$ " + valorFinal2);
+    
+    
+}
+
+else{
+    console.log("MÉTODO DE PAGAMENTO CRÉDTIO")
+    let parcelas = 0
+
+    if(parcelamento == 1){
+        console.log("Avista " +valorGasto);
+        
+    }else if(parcelamento == 2){
+        parcelas = (valorGasto * 1.05) / 2
+        console.log("parcelas em 2x " +parcelas );
+        
+    }else{
+        parcelas = (valorGasto * 1.10) / 3
+        console.log("parcelas em 3x" +parcelas );
+
+    }
+    
+   
+}
