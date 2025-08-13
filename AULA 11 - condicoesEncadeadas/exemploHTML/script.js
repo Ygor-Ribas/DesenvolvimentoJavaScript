3163c0777d0af6293c88278bc65c6f652d9bdfac
Ygor-Ribas <moreiraribasy@gmail.com>
2025-08-13 16:33:38 -0300

    .

---
+++ AULA 11 - condicoesEncadeadas/exemploHTML/script.js
@@ -0,0 +1,47 @@
+function mostrarResultado(){
+    let nota1 = parseInt(document.querySelector("#nota1").value)
+    let nota2 = parseInt(document.querySelector("#nota2").value)
+    let notaRecuperacao = document.querySelector("#notaRecuperacao")
+    let resultadoMedia = document.querySelector("#media")
+    let resultado = document.querySelector("#resultado")
+
+    let media = (nota1 + nota2) / 2
+    resultadoMedia.innerHTML = "A sua media é de " +media
+    
+    if (media >= 70){
+        resultado.innerHTML = "Aprovado"
+        resultado.style.color = "green"
+        
+        
+    }else if(media >= 50){
+        let valorNotaRecuperacao = parseInt(notaRecuperacao.value) 
+        let mediaFinal = (media + valorNotaRecuperacao) / 2
+        resultadoMedia.innerHTML = "A sua media é de " +media
+        
+        
+        
+        
+        
+        
+        if (isNaN(valorNotaRecuperacao)) {
+            resultado.innerHTML = "Recuperação"
+            resultado.style.color = "orange"
+            notaRecuperacao.style.display = "block"
+            
+        }
+        
+        
+        
+        else if(mediaFinal >= 70 ){
+            resultadoMedia.innerHTML = "A sua media é de " +mediaFinal
+            resultado.innerHTML = "Aprovado"
+            resultado.style.color = "green"
+            
+        }else{
+            resultadoMedia.innerHTML = "A sua media é de " +mediaFinal
+             resultado.innerHTML = "Reprovado"
+             resultado.style.color = "red"
+
+        }
+    }
+}
