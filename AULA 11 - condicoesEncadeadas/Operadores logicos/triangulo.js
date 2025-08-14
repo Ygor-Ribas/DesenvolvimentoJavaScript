88f80a3ea5a81056ab454a7d145b0bd2f1fa04fb
Ygor-Ribas <moreiraribasy@gmail.com>
2025-08-14 16:24:58 -0300

    .

--- AULA 11 - condicoesEncadeadas/Operadores logicos/triangulo.js
+++ AULA 11 - condicoesEncadeadas/Operadores logicos/triangulo.js
@@ -1,21 +1,30 @@
-let ladoA = 20
-let ladoB = 30
-let ladoC = 10
+function mostrarResultado (){
+
+let ladoA = parseInt(document.querySelector("#ladoA").value)
+let ladoB = parseInt(document.querySelector("#ladoB").value)
+let ladoC = parseInt(document.querySelector("#ladoC").value)
+let resultado = document.querySelector("#resultado") 
 
 
 
 
-
+ let resultados = 0
 if (ladoA == ladoB && ladoB == ladoC ) {
     console.log("Este é um triangulo Equilátero");
+    
+    resultado.innerHTML = "Este é um triangulo Equilátero" 
    
 }else if (ladoA == ladoB  || ladoA == ladoC || ladoB == ladoC){
     console.log("Este é um triangulo Isósceles");
     
+    resultado.innerHTML = "Este é um triangulo Isósceles"  
+    
 }else{
     console.log("Este é um triangulo Escaleno");
     
+    resultado.innerHTML = "Este é um triangulo Escaleno"    
     
     
     
+}
 }
