e3391e956d72e95fe38a6ddd96ff2bcecd5e2829
Ygor-Ribas <moreiraribasy@gmail.com>
2025-08-20 16:54:04 -0300

    .

---
+++ AULA 13 - Arrays/html/script.js
@@ -0,0 +1,27 @@
+let tarefas =[]
+
+function adicionarTarefa(){
+    let tarefa = document.getElementById("nova-tarefa").value 
+
+    if (tarefas) {
+        tarefas.push(tarefa)
+        exibeTarefas()
+        tarefa = ""
+        
+    }
+    console.log(tarefas);
+    
+
+}
+
+function exibeTarefas(){
+    let listaTarefas = document.getElementById("lista-tarefas")
+
+    listaTarefas.forEach((tarefa) => {
+        let li = document.createElement("li")
+        li.innerHTML = tarefa
+        listaTarefas.appendChild(li)
+
+    })
+}
+
